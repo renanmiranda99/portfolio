400d652087c7e8a39b71656dc0b9076f8b888bd5
Renan Miranda <132950877+renanmiranda99@users.noreply.github.com>
2025-03-20 13:06:21 -0300

    Update texto on AboutContainer.jsx

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,12 +4,12 @@ const AboutContainer = () => {
       <h2>Sobre</h2>
       <p>
       Olá, sou Renan!
-      Atualmente estou no 3º período do curso de Análise e Desenvolvimento de Sistemas, que iniciei em 2023. 
+      Atualmente estou no 4º período do curso de Análise e Desenvolvimento de Sistemas, que iniciei em 2023. 
       Estou iniciando minha jornada acadêmica através do programa Embarque Digital, uma iniciativa em parceria 
       da Prefeitura do Recife e o Porto Digital, o maior parque tecnológico urbano e aberto do Brasil.
       </p>
       <p>
-      Sou um entusiasta da tecnologia, apaixonado principalmente pelas áreas de desenvolvimento back-end e 
+      Sou um entusiasta da tecnologia, apaixonado principalmente pelas áreas de desenvolvimento back-end, front-end e 
       games. Estou ansioso para aprender mais, crescer profissionalmente e contribuir para o mundo da tecnologia.
       </p>
     </section>
